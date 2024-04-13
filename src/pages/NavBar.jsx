def3c7b0c185ce86bf24c50c22404c39d4508f42
nishanpradhan <nishanpradhan500@gmail.com>
2024-04-13 17:22:38 +0545

    bug-fix-a

--- src/pages/NavBar.jsx
+++ src/pages/NavBar.jsx
@@ -33,7 +33,7 @@ const NavBar = () => {
     return (
         <nav className={isSticky ? 'sticky' : ''}>
             <div className="navbar">
-                <div className="logo"><a href="#">Nishan.</a></div>
+                <div className="logo"><a href="#home">Nishan.</a></div>
                 <ul className={`menu ${isMenuOpen ? 'active' : ''}`}>
                     <li><a href="#home" onClick={closeMenu}>Home</a></li>
                     <li><a href="#about" onClick={closeMenu}>About</a></li>
@@ -46,17 +46,18 @@ const NavBar = () => {
                     </div>
                 </ul>
                 <div className="media-icons">
-                    <a href="https://www.facebook.com/profile.php?id=100015085275074" target="_blank"><i className="fab fa-facebook-f"></i></a>
-                    <a href="https://twitter.com/home" target="_blank"><i className="fab fa-twitter"></i></a>
-                    <a href="https://github.com/Nishan-Pradhan06" target="_blank"><i className="fab fa-github"></i></a>
-                    <a href="https://www.instagram.com/pradhan_nishan_16/?hl=en" target="_blank"><i className="fab fa-instagram"></i></a>
-                    <a href="https://www.linkedin.com/in/nishan-pradhan06/" target="_blank"><i className="fab fa-linkedin"></i></a>
+                    <a href="https://www.facebook.com/profile.php?id=100015085275074" target="_blank" rel="noopener noreferrer"><i className="fab fa-facebook-f"></i></a>
+                    <a href="https://twitter.com/home" target="_blank" rel="noopener noreferrer"><i className="fab fa-twitter"></i></a>
+                    <a href="https://github.com/Nishan-Pradhan06" target="_blank" rel="noopener noreferrer"><i className="fab fa-github"></i></a>
+                    <a href="https://www.instagram.com/pradhan_nishan_16/?hl=en" target="_blank" rel="noopener noreferrer"><i className="fab fa-instagram"></i></a>
+                    <a href="https://www.linkedin.com/in/nishan-pradhan06/" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin"></i></a>
                 </div>
             </div>
             <div className="menu-btn" onClick={toggleMenu}>
                 <i className="fas fa-bars"></i>
             </div>
         </nav>
+
     );
 };
 
