f15baef4e67dafee142ebb49383ebf27c6107164
Rupee88 <chyrupesh828@gmail.com>
2025-05-12 10:59:44 +0545

    added changes in portfolio

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
